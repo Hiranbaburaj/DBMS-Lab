dd15bb5f188cbdf8d81f68329ee51c0d29e1e198
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-04-11 12:07:00 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,7 +1,7 @@
 create database bank;
 use bank;
 
-create table branch(branch_name varchar(20) primary key, branch_city varchar(20), assets varchar(100));
+create table branch(branch_name varchar(20) primary key, branch_city varchar(20), assets numeric(10,5));
 create table customer(customer_name varchar(20) primary key,customer_street varchar(20), customer_city varchar(20));
 create table loan(loan_no varchar(10) primary key, branch_name varchar(20), amount numeric(8,3), foreign key(branch_name) references branch(branch_name));
 create table borrower(customer_name varchar(20),loan_no varchar(10),primary key(customer_name,loan_no));
@@ -14,3 +14,10 @@ create table depositor(customer_name varchar(20),account_number varchar(10),
 	primary key(customer_name,account_number),
     foreign key(customer_name) references customer(customer_name),
     foreign key(account_number) references account(account_number));
+   
+   
+INSERT INTO branch VALUES ('Peroorkada','Trivandrum', 2574.34);  
+INSERT INTO branch VALUES ('Pattoor','Trivandrum', 65716.97);    
+INSERT INTO branch VALUES ('Chinnakada','Kollam', 15796.31);  
+INSERT INTO branch VALUES ('Vyttila','Ernakulam', 87423.54);  
+INSERT INTO branch VALUES ('Perumbavoor','Ernakulam', 97311.02);  
