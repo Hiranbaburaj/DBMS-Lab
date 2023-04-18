9f5de1eb7c2cc2e9c31e215b34cc9324b13a75aa
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-04-18 13:53:53 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,16 +1,16 @@
+drop database bank;
 create database bank;
 use bank;
-
 create table branch(branch_name varchar(20) primary key, branch_city varchar(20), assets numeric(10,5));
 create table customer(customer_name varchar(20) primary key,customer_street varchar(20), customer_city varchar(20));
-create table loan(loan_no varchar(10) primary key, branch_name varchar(20), amount numeric(8,3), foreign key(branch_name) references branch(branch_name));
+create table loan(loan_no varchar(10) primary key, branch_name varchar(20), amount numeric(15,3), foreign key(branch_name) references branch(branch_name));
 create table borrower(customer_name varchar(20),loan_no varchar(10),primary key(customer_name,loan_no));
 
 alter table borrower add foreign key(customer_name) references customer(customer_name);
 alter table borrower add foreign key(loan_no) references loan(loan_no);
 
-create table account(account_number int(10) primary key,branch_name varchar(20),balance numeric(8,3),foreign key(branch_name) references branch(branch_name));
-create table depositor(customer_name varchar(20),account_number int(10),
+create table account(account_number int primary key,branch_name varchar(20),balance numeric(15,3),foreign key(branch_name) references branch(branch_name));
+create table depositor(customer_name varchar(20),account_number int,
 	primary key(customer_name,account_number),
     foreign key(customer_name) references customer(customer_name),
     foreign key(account_number) references account(account_number));
@@ -35,4 +35,16 @@ INSERT INTO borrower VALUES ('Jake', 'A1');
 INSERT INTO loan VALUES ('B1', 'Chinnakada', 13000.0);
 INSERT INTO borrower VALUES ('Smith', 'B1');
 
-INSERT INTO account VALUES ('B1', 'Chinnakada', 13000.0);
+INSERT INTO account VALUES (23715046, 'Pattoor', 1789451.46);
+INSERT INTO account VALUES (54782069, 'Peroorkada', 54213.67);
+INSERT INTO account VALUES (78400656, 'Chinnakada', 213654.97);
+INSERT INTO account VALUES (23050047, 'Chinnakada', 5412369.21);
+INSERT INTO account VALUES (10032478, 'Vyttila', 97215.34);
+INSERT INTO account VALUES (55400871, 'Vyttila', 3456.21);
+
+INSERT INTO depositor VALUES ('Jake', 23715046);
+INSERT INTO depositor VALUES ('Ronald', 54782069);
+INSERT INTO depositor VALUES ('Smith', 78400656);
+INSERT INTO depositor VALUES ('Neethu', 23050047);
+INSERT INTO depositor VALUES ('Raju', 10032478);
+INSERT INTO depositor VALUES ('Basha', 55400871);
